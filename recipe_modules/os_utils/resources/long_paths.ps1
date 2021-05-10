c1ca2b4cda726c57a203593117b0436d7b8e123b
Chris Bracken <cbracken@google.com>
2021-05-10 23:12:05 +0000

    Better logging in long_paths.ps1
    Emit more detailed logs about previous and new state of LongPathsEnabled
    registry value to aid in future debugging.
    
    Bug: https://github.com/flutter/flutter/issues/82184
    Change-Id: I0d830a289ee79cddb295bd05b93c2f7fb2540520
    Reviewed-on: https://flutter-review.googlesource.com/c/recipes/+/13361
    Reviewed-by: Zach Anderson <zra@google.com>
    Reviewed-by: Godofredo Contreras <godofredoc@google.com>
    Commit-Queue: Chris Bracken <cbracken@google.com>

--- recipe_modules/os_utils/resources/long_paths.ps1
+++ recipe_modules/os_utils/resources/long_paths.ps1
@@ -6,21 +6,23 @@ $filesystemKey = "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem"
 
 if (Get-ItemProperty -Path $filesystemKey -Name "LongPathsEnabled") {
   # Key is already set, if it's not 1, set it
-  echo "LongPathsEnabled is already set on machine"
   $longPathsEnabled = Get-ItemPropertyValue `
       -Path "$filesystemKey" `
       -Name "LongPathsEnabled"
   if (-not $longPathsEnabled) {
+    echo "LongPathsEnabled is currently 0. Setting to 1"
     Set-ItemProperty `
       -Path "$filesystemKey" `
       -Name "LongPathsEnabled" `
       -Value 1 `
       -Force
+  } else {
+    echo "LongPathsEnabled is currently 1. Not modifying."
   }
 
 } else {
   # LongPathsEnabled value is not yet present, set it
-  echo "LongPathsEnabled is not yet set on machine; setting."
+  echo "LongPathsEnabled is not yet set on machine; setting to 1."
   New-ItemProperty `
       -Path "$filesystemKey" `
       -Name "LongPathsEnabled" `
@@ -33,4 +35,4 @@ if (Get-ItemProperty -Path $filesystemKey -Name "LongPathsEnabled") {
 $longPathsEnabled = Get-ItemPropertyValue `
     -Path "$filesystemKey" `
     -Name "LongPathsEnabled"
-echo "LongPathsEnabled is $longPathsEnabled"
+echo "LongPathsEnabled is set to $longPathsEnabled"
